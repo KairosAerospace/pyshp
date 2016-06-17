bb95ba5167ed16b1f83f2c8a2ca91501d3ef382f
Ari Gesher <ari@gesher.net>
2016-06-17 15:44:09 -0700

    Changed name for insertion into our package index

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
-setup(name='pyshp',
+setup(name='kairos-pyshp',
       version='1.2.3',
-      description='Pure Python read/write support for ESRI Shapefile format',
+      description='Pure Python read/write support for ESRI Shapefile format (repackaged with bugfixes for Kairos)',
       long_description=open('README.txt').read(),
       author='Joel Lawhead',
       author_email='jlawhead@geospatialpython.com',
       url='https://github.com/GeospatialPython/pyshp',
-      download_url='https://github.com/GeospatialPython/pyshp/archive/1.2.2.tar.gz',
+      download_url='https://github.com/GeospatialPython/pyshp/archive/1.2.3.tar.gz',
       py_modules=['shapefile'],
       license='MIT',
       zip_safe=False,
