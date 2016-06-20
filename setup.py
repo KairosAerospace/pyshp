ac17c95631ce054140c6fbd88f94e6ff6d4c597f
JP Addison <johnpaddison@gmail.com>
2016-06-20 14:01:36 -0700

    circle 2

--- setup.py
+++ setup.py
@@ -1,4 +1,21 @@
+import sys
 from setuptools import setup
+
+
+# make sure we don't accidentally publish to PyPI
+# See: https://www.tomaz.me/2013/09/03/prevent-accidental-publishing-of-a-private-python-package.html
+def forbid_publish():
+    argv = sys.argv
+    blacklist = ["register"]
+
+    for command in blacklist:
+        if command in argv:
+            values = {"command": command}
+            print(("Command '%(command)s' has been blacklisted, exiting..." %
+                  values))
+            sys.exit(2)
+
+forbid_publish()
 
 setup(name='kairos-pyshp',
       version='1.2.3',
@@ -16,18 +33,3 @@ setup(name='kairos-pyshp',
                    'Topic :: Scientific/Engineering :: GIS',
                    'Topic :: Software Development :: Libraries',
                    'Topic :: Software Development :: Libraries :: Python Modules'])
-
-# make sure we don't accidentally publish to PyPI
-# See: https://www.tomaz.me/2013/09/03/prevent-accidental-publishing-of-a-private-python-package.html
-def forbid_publish():
-    argv = sys.argv
-    blacklist = ["register"]
-
-    for command in blacklist:
-        if command in argv:
-            values = {"command": command}
-            print(("Command '%(command)s' has been blacklisted, exiting..." %
-                  values))
-            sys.exit(2)
-
-forbid_publish()
